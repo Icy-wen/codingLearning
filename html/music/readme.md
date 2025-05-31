5534cb94493e09fc386132703f4c6965577575c9
wenbing <wb13576484183@163.com>
2025-05-31 23:37:44 +0800

    还原设计稿

---
+++ html/music/readme.md
@@ -0,0 +1,7 @@
+# flex 弹性布局
+1. display: flex; 弹性容器可以控制内部子元素的排列方式
+2. 子容器默认是水平排列的
+3. justify-content: center; 控制子容器水平居中
+4. align-items: center; 控制子容器垂直居中
+5. flex-direction: column; 控制子容器垂直排列
+6. 弹性容器可以按子容器分配空间
--- js/预编译/2.js
+++ js/预编译/2.js
@@ -1,7 +1,7 @@
+var c = 1; // 全局作用域
 function foo(a, b) {
   console.log(a);
-  c = 0
-  var c;
+ 
   a = 3
   b = 2
   console.log(b);
@@ -9,3 +9,4 @@ function foo(a, b) {
   console.log(b);
 }
 foo(1)
+console.log(c)
