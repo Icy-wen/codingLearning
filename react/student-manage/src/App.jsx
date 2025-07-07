962fb818e821bf34be02806c30959f62fff4297f
wen <2584346389@qq.com>
2025-07-07 23:12:08 +0800

    管理系统

--- react/student-manage/src/App.jsx
+++ react/student-manage/src/App.jsx
@@ -1,13 +1,18 @@
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import Login from './views/login';
 import Layout from './views/layout';
+import Home from './views/home';
+import Publish from './views/publish';
 function App(){
     return (
         <div>
             <BrowserRouter>
                 <Routes>
                     <Route path="/login" element={<Login/>}></Route>
-                    <Route path="/layout" element={<Layout/>}></Route>
+                    <Route path="/layout" element={<Layout/>}>
+                    <Route path='' element={<Home/>}></Route>
+                    <Route path='publish' element={<Publish/>}></Route>
+                    </Route>
                 </Routes>
             </BrowserRouter>
         </div>
--- react/student-manage/src/views/layout/index.jsx
+++ react/student-manage/src/views/layout/index.jsx
@@ -1,8 +1,75 @@
+import React, { useState } from 'react';
+import './index.css'
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Button, Layout, Menu, theme } from 'antd';
+import { Link ,Outlet} from 'react-router-dom';
+
 //页面布局
-function Layout(){
+function MainLayout(){
+    const { Header, Sider, Content } = Layout;
+     const [collapsed, setCollapsed] = useState(false);
+     const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
     return(
-        <h2>Layout</h2>
+        <Layout className='layout'>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: <Link to='/layout'>首页</Link>,
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: <Link to='/layout/publish'>信息发布</Link>,
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: '内容管理',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout >
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content className='layout-content'
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <Outlet/>
+        </Content>
+      </Layout>
+    </Layout>
     )
 }
 
-export default Layout
+export default MainLayout
